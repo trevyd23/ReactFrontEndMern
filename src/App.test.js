6192431b801f19ed56c248c1cb5d63bd6866244d
tredon <trevidlas@gmail.com>
2021-04-05 21:04:31 -0500

    Configure testing environment and downgraded react version

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App.jsx';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
@@ -59,6 +59,8 @@ jest.mock('./cssStyles/shoppingList.css', () => {
   }
 })
 describe('renders main app component', () => {
+
+  afterEach(cleanup)
 
   it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
